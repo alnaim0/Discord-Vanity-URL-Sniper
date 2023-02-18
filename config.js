6dc3ae9a3faa8148628f96b187f38285b54bcaf3
alnaim0 <125700592+alnaim0@users.noreply.github.com>
2023-02-18 19:13:11 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {
-  TOKEN: "", //write your discord bot token
-  authorID: "", //write your discord account id.
-  url: "", //Type the Custom Discord Server URL You Want to Retrieve!
-  serverID: "", //Type the Server ID to Get the Url!
-  log: "", //If the URL is taken, write the Log Channel ID to be informed!
+  TOKEN: "ODg2OTA3MDE5MTM3Nzc3NjY0.Go4ydl.kP51RHBetefi4OySsNevl985YoAAYjQkVJt5aM", //write your discord bot token
+  authorID: "573895046269763604", //write your discord account id.
+  url: "027", //Type the Custom Discord Server URL You Want to Retrieve!
+  serverID: "https://discord.gg/0271", //Type the Server ID to Get the Url!
+  log: "https://canary.discord.com/api/webhooks/1076253944839409764/IwGm4heud758ifWS8oAdJiq_UIAvrBYr0ZcJdQfP5fC-yN1DHBcW7eM-NqxeKdoypIQa", //If the URL is taken, write the Log Channel ID to be informed!
   status: "true", //false=Stop Bot | true=Means Start the Bot!
   botRun: "10" //1000 = 1 Second | 1 = 1 Millisecond | It is recommended to write at least 50 milliseconds. Under 30MS, the project will be banned from the Discord API and will stop working!
 
